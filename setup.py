6ad4942b4a4964f138f6b096b5dd1d9878df3dd6
ABED TOUFIK <124655855+ABEDToufikSK@users.noreply.github.com>
2023-10-17 09:24:12 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PySigmaKoki',
+    name='SigmaKokiPy',
     version='0.0.1',
     description='Control Sigma Koki Controllers/Motorized Stages including SHOT/Hit/FC mode',
-    url='https://github.com/ABEDToufikSK/PySigmaKoki.git',
-    author='ABEDToufik88',
-    author_email='abedtoufik.g@gmail.com',
+    url='https://github.com/ABEDToufikSK/SigmaKokiPy.git',
+    author='ABED Toufik',
+    author_email='abedtoufik.g@gmail.com''t.abed@sigma-koki.com',
     license='MIT',
     install_requires=['pyserial','enum','time'],
     python_requires='>=3.2',  #  Python versions supported
